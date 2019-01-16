5236c71a984446c9930cf80af05c4ee09e001915
Phạm Gia Vang <phamgiavang@gmail.com>
2019-01-17 01:09:37 +0700

    Create DDC.php

---
+++ DDC.php
@@ -0,0 +1,9 @@
+<?php
+//Please Don't Rename This File//
+
+$config['token']  = 'xxxxxxx';
+$config['fromUserId'] = 'xxxxxx';
+$config['userId']  = 'xxxxx';
+$config['accessKeyId']  = 'xxxxxx';
+$config['nonce']  = 'xxxxx';
+$config['deviceId']  = 'xxxxx';
